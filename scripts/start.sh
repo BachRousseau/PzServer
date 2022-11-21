c6a1c723e247c14d942e27dc34ddacdad5a29d31
bachrousseau <hbec53@gmail.com>
2022-11-21 18:32:01 -0500

    last change

--- scripts/start.sh
+++ scripts/start.sh
@@ -38,14 +38,14 @@ echo "---Update Server---"
 if [ "${STEAM_USER}" == "" ]; then
     	echo "---Validating installation---"
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
+        +force_install_dir ${PZ_SERVER_DIR} \
         +login anonymous \
         +app_update ${GAME_ID} validate \
         +quit
 else
     	echo "---Validating installation---"
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
+        +force_install_dir ${PZ_SERVER_DIR} \
         +login ${STEAM_USER} ${PASSWRD} \
         +app_update ${GAME_ID} validate \
         +quit
@@ -53,28 +53,28 @@ fi
 
 echo "---Prepare Server---"
 echo "---Setting up Environment---"
-export PATH="${SERVER_DIR}/jre64/bin:$PATH"
-export LD_LIBRARY_PATH="${SERVER_DIR}/linux64:${SERVER_DIR}/natives:${SERVER_DIR}:${SERVER_DIR}/jre64/lib/amd64:${LD_LIBRARY_PATH}"
-export JSIG="libjsig.so"
-export JARPATH="java/:java/lwjgl.jar:java/lwjgl_util.jar:java/sqlite-jdbc-3.8.10.1.jar:java/uncommons-maths-1.2.3.jar"
+# export PATH="${PZ_SERVER_DIR}/jre64/bin:$PATH"
+# export LD_LIBRARY_PATH="${PZ_SERVER_DIR}/linux64:${PZ_SERVER_DIR}/natives:${PZ_SERVER_DIR}:${PZ_SERVER_DIR}/jre64/lib/amd64:${LD_LIBRARY_PATH}"
+# export JSIG="libjsig.so"
+# export JARPATH="java/:java/lwjgl.jar:java/lwjgl_util.jar:java/sqlite-jdbc-3.8.10.1.jar:java/uncommons-maths-1.2.3.jar"
 echo "---Looking for server configuration file---"
-if [ ! -d ${SERVER_DIR}/Zomboid ]; then
+if [ ! -d ${PZ_SERVER_DIR}/Zomboid ]; then
 	echo "---No server configruation found, downloading template---"
-	cd ${SERVER_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll https://github.com/ich777/docker-steamcmd-server/raw/projectzomboid/config/cfg.zip ; then
+	cd ${PZ_SERVER_DIR}
+	if wget -q -nc --show-progress --progress=bar:force:noscroll https://github.com/BachRousseau/PzServer/raw/e298e663c54ccbd55f1938c9ed20d0b23ffe054b/Server/serverconfig.zip ; then
 		echo "---Sucessfully downloaded server configuration file---"
 	else
 		echo "---Something went wrong, can't download server configuration file, putting server in sleep mode---"
 		sleep infinity
 	fi
-	unzip -o ${SERVER_DIR}/cfg.zip
-	rm ${SERVER_DIR}/cfg.zip
+	unzip -o ${PZ_SERVER_DIR}/cfg.zip
+	rm ${PZ_SERVER_DIR}/cfg.zip
 else
 	echo "---Server configuration files found!---"
 fi
 
 echo "---Checking for old logs---"
-find ${SERVER_DIR} -name "masterLog.0" -exec rm -f {} \; > /dev/null 2>&1
+find ${PZ_SERVER_DIR} -name "masterLog.0" -exec rm -f {} \; > /dev/null 2>&1
 chmod -R 770 ${DATA_DIR}
 echo "---Server ready---"
 echo "---Symbolic links---"
