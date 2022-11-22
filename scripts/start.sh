41fa7681fb291567e72c1137f95d9aa1637ad166
bachrousseau <hbec53@gmail.com>
2022-11-21 19:24:12 -0500

    last change2

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "---Instalando dependencias---"
- dpkg --add-architecture i386; apt update;  apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 openjdk-17-jre-headless ;  apt upgrade 
+ dpkg --add-architecture i386; apt update;  apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 openjdk-17-jre-headless ;  apt upgrade 
 
 adduser ${DIST_USER}
 
