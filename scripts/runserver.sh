83d92fc60d6521317375aefb57d1b19110f763e5
bachrousseau <hbec53@gmail.com>
2022-11-22 20:34:39 -0500

    last change2

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/data/pzserver/start-server.sh +password ${PZ_ADMIN_PASS} -servername ${PZ_SV_NAME} -port ${PZ_PORT} -udpport ${PZ_PORT_2} -steamport1 ${STEAM_PORT} -Xmx ${PZ_RAM} 
+/data/pzserver/start-server.sh -adminpassword ${PZ_ADMIN_PASS} -servername ${PZ_SV_NAME} -port ${PZ_PORT} -udpport ${PZ_PORT_2} -steamport1 ${STEAM_PORT} -Xmx ${PZ_RAM} 
